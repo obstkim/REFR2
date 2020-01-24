1e1bfc48c54945014094a9552d0c5ebf3cae615d
obstkim <obstkim@yahoo.de>
2020-01-24 08:54:07 +0000

    test

---
+++ LED/ledL.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <time.h>
+
+#include "ledL.c"
+
+void blink(char*);
---
+++ LED/main.c
@@ -0,0 +1,6 @@
+#include "ledL.h"
+
+int main (int argc, char **arg)
+{
+   blink(arg[1]);
+}
